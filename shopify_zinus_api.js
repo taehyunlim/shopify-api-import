9400a0815d2def9c639d5cf9e98101c80ed8db06
taehyunlim <taehyun.lim@gmail.com>
2017-09-12 17:35:27 -0700

    Write function for Import file added

--- shopify_zinus_api.js
+++ shopify_zinus_api.js
@@ -17,52 +17,69 @@ var apikey = config.shopify_api_key;
 var password = config.shopify_api_pw;
 var shopname = config.shopify_shopname;
 
-// Global Variables
+// Global variables
 var baseurl = 'https://'+apikey+':'+password+'@'+shopname+'.myshopify.com';
-//var attrFields = '';
-var csvFields = ['created_at', 'id', 'name', 'total_price'];
-var j2cOptions = { keys: csvFields };
 var timestring = moment().format("YYYYMMDD_HHmm");
-var fileName = './Incoming/ShopifyOrders_' + timestring +'.csv';
-var lastImportOrderIdCsv = 'lastImport.csv'
+var incomingPathName = './Incoming/' //'ShopifyOrders_' + timestring +'.csv';
+var incomingFileName = 'ShopifyAPI_Orders_'
+var importPathName = './Import/' //'OE_NewOrder_' + timestring + '_ZINUS.csv';
+var importFileName = 'OE_NewOrder_'
+var lastImportCsv = 'lastImport.csv'
 var lastImportOrderIdStart = '';
 var lastImportOrderIdEnd = '';
+var lastDocumentIdStart = '';
+var lastDocumentIdEnd = '';
 
-// Recall last imported order_number
-function recallOrderNo(callback) {
-  fs.readFile(lastImportOrderIdCsv, function(err, fileData){
+// Recall last imported orderId
+function recallOrderId(callback) {
+  fs.readFile(lastImportCsv, function(err, fileData){
     if (err) {
-      // Base case when lastImportOrderId DNE
+      // Base case when lastImportCsv DNE
       if (err.code === 'ENOENT') {
         console.error('No lastImport found. Creating lastImport.csv');
-        fs.writeFile(lastImportOrderIdCsv, "", function(err) {
+        fs.writeFile(lastImportCsv, "", function(err) {
           if (err) throw err;
         });
-        // Set initial value for lastImportOrderId to 0;
-        fileData = '0';
+        // Set initial value for lastImportOrderId: 0 & lastDocumentId: 0;
+        fileData = '0, 0';
       } else { throw err; }
     }
     parse(fileData, function(err, output) {
       if (err) throw err;
+      // Load lastImportOrderId and lastDocumentId
       lastImportOrderIdStart = output[0][0];
+      lastDocumentIdStart = output[0][1];
+      console.log('Check lastDocumentIdStart: ' + lastDocumentIdStart);
       // Run Shopify API call to get orders
       getOrders(lastImportOrderIdStart);
     });
   });
 }
 
-// Callback function for json-2-csv
-function j2cCallback(err, csv) {
+// Callback function for json-2-csv: Incoming API result file
+function j2cCallbackIncoming(err, csv) {
+  // Update ending lastDocumentId
+  lastDocumentIdEnd = parseInt(lastDocumentIdStart) + 1;
   if (err) throw err;
-  // Generate csv for OMDB import
-  fs.writeFile(fileName, csv, function(err) {
+  // Generate csv for Incoming Directory
+  fs.writeFile(incomingPathName + incomingFileName + timestring + '.csv', csv, function(err) {
     if (err) throw err;
-    console.log('File saved under: [' + __dirname + ']@[' + timestring + ']');
+    console.log('File saved under: [' + __dirname + '\\Incoming\\]@[' + timestring + ']');
   });
   // Record last range of order_number to lastImport.csv
-  fs.writeFile(lastImportOrderIdCsv, lastImportOrderIdEnd, function(err) {
+  fs.writeFile(lastImportCsv, lastImportOrderIdEnd + ',' + lastDocumentIdEnd, function(err) {
     if (err) throw err;
   })
+}
+
+// Callback function for json-2-csv: Import file
+function j2cCallbackImport(err, csv) {
+  if (err) throw err;
+  // Generate csv for Incoming Directory
+  fs.writeFile(importPathName + importFileName + timestring + '.csv', csv, function(err) {
+    if (err) throw err;
+    console.log('File saved under: [' + __dirname + '\\Import\\]@[' + timestring + ']');
+  });
 }
 
 // Main API Request Call function
@@ -84,20 +101,18 @@ function getOrders(orderId) {
       if (!error && response.statusCode === 200 && body.orders.length > 0) {
         // Objects Array for Orders
         var ordersList = [];
+        var cart_subtotal_list = [];
         // Nested loop through line_items (ln) and order objects (ord)
         for (var i = 0; i < body.orders.length; i++) {
-
-          // BEGIN TEST: CART LEVEL DISCOUNT CODE
-          if (body.orders[i].discount_codes[0] != null ) {
-            // HARDCODED: ONLY ONE DISCONT CODE TAKEN
-            var discount_codes_amount_test = body.orders[i].discount_codes[0].amount;
-          } //END TEST
-
+          // Variable for cart subtotal loop
+          var cart_subtotal_helper = 0;
           // Rename iteratee to "ord"
           var ord = body.orders[i];
           for (var j = 0; j < ord.line_items.length; j++) {
             // Declare empty object var for order object "ordObj"
             var orderObj = {
+              documentNo: parseInt(lastDocumentIdStart) + 1,
+              documentTime: timestring,
               order_index: 0,
               shopifyOrderId: '',
               shipping_address_name: '',
@@ -116,12 +131,23 @@ function getOrders(orderId) {
               total_line_items_price: '',
               subtotal_price: '',
               total_tax: '',
+              discount_codes_code: '',
+              discount_codes_type: '',
               discount_codes_amount: '',
+              discount_fixed_amount: '',
               total_discounts: '',
               line_items_index: 0,
               line_items: {
                 tax_price: '',
                 tax_rate: ''
+              },
+              cart_pricing: {
+                regular_price: '',
+                cart_price: '',
+                cart_subtotal: '',
+                cart_discount_percent_rate: '',
+                cart_discount_percent_amount: '',
+                unit_price: '',
               }
             };
             // Rename iteratee to "ln"
@@ -145,7 +171,19 @@ function getOrders(orderId) {
             orderObj.total_line_items_price = ord.total_line_items_price;
             orderObj.subtotal_price = ord.subtotal_price;
             orderObj.total_tax = ord.total_tax;
-            orderObj.discount_codes_amount = discount_codes_amount_test; // HARDCODED
+            if (ord.discount_codes[0] != null) { // HARDCODED: ONLY TAKES A SINGLE DISCOUNT CODE PER ORDER
+              orderObj.discount_codes_amount = ord.discount_codes[0].amount;
+              // IF COUPON IS PERCENT BASED
+              if (ord.discount_codes[0].type === "percentage") {
+                orderObj.discount_codes_type = "percentage";
+                orderObj.discount_codes_code = ord.discount_codes[0].code;
+                // COUPON PERCENTAGE IS EXTRACTED FROM COUPON CODE LAST 2 DIGIT (e.g. COUPON10 => 10%)
+                orderObj.cart_pricing.cart_discount_percent_rate = parseInt(orderObj.discount_codes_code.trim().slice(-2))/100;
+              } else if (ord.discount_codes[0].type === "fixed_amount") {
+                orderObj.discount_codes_type = "fixed_amount";
+                orderObj.discount_fixed_amount = orderObj.discount_codes_amount;
+              }
+            }
             orderObj.total_discounts = ord.total_discounts;
             orderObj.line_items_index = j + 1; // 1-based index
             // Assign ln values
@@ -153,25 +191,124 @@ function getOrders(orderId) {
             orderObj.line_items.qty = ln.quantity;
             orderObj.line_items.price = ln.price;
             orderObj.line_items.recycling_fee = "Test";
-            orderObj.line_items.discount = ln.total_discount;
+            orderObj.line_items.total_discount = ln.total_discount;
             // HARDCODED: ONLY TAKES SINGLE TAX LINE PER LINE ITEM
             if (ln.tax_lines.length > 0) {
               orderObj.line_items.tax_price = ln.tax_lines[0].price
               orderObj.line_items.tax_rate = ln.tax_lines[0].rate
             }
+            // Cart price calculation for OMDB
+            orderObj.cart_pricing.regular_price = orderObj.line_items.price;
+            orderObj.cart_pricing.cart_price = orderObj.line_items.price - (orderObj.line_items.total_discount)/(orderObj.line_items.qty);
+            cart_subtotal_helper = cart_subtotal_helper + (orderObj.cart_pricing.cart_price*orderObj.line_items.qty);
+            orderObj.cart_pricing.cart_subtotal = cart_subtotal_helper
+
             // Add assigned object to ordersList array
             ordersList.push(orderObj);
           } // end of loop: line_items
+
+          // Cart subtotal calcuation
+          var subtotalObj = {
+            order_index: i+1,
+            subtotal: cart_subtotal_helper
+          }
+          cart_subtotal_list.push(subtotalObj);
+
         } // end of loop: orders
+
+        // Cart subtotal calcuation
+        ordersList.forEach(function(order){
+          // Callback function for find()
+          function findSubtotal(e){
+            return e.order_index === order.order_index
+          };
+          // Return subtotal for the given order index
+          order.cart_pricing.cart_subtotal = (cart_subtotal_list.find(findSubtotal).subtotal);
+          if (order.discount_codes_type === "percentage") {
+            // Discount amount based on percent
+            order.cart_pricing.cart_discount_percent_amount = order.cart_pricing.cart_price*(order.cart_pricing.cart_discount_percent_rate);
+            // Unit price is the final price sold for each item
+            order.cart_pricing.unit_price = order.cart_pricing.cart_price*(1-order.cart_pricing.cart_discount_percent_rate);
+          } else {
+            // Unit price equals the cart price when there is not percent based discount
+            order.cart_pricing.unit_price = order.cart_pricing.cart_price;
+          }
+        });
+
         console.log('order count: ' + ordersList[ordersList.length-1].order_index);
         console.log('Ending Order_number: ' + ordersList[ordersList.length-1].zinus_po);
         console.log('Ending lastImportOrderId: ' + ordersList[ordersList.length-1].shopifyOrderId);
         lastImportOrderIdEnd = ordersList[ordersList.length-1].shopifyOrderId;
 
-        // Convert json objects to csv
-        j2c.json2csv(ordersList, j2cCallback);
+        // Convert json objects to csv and write in Incoming
+        j2c.json2csv(ordersList, j2cCallbackIncoming);
+
+        // Create order import object for import file
+        var importList = [];
+        for (var i = 0; i < ordersList.length; i++) {
+          var orderImport = ordersList[i]
+          var orderImportObjCopy = {};
+          orderImportObjCopy.ISACONTROLNO = orderImport.shopifyOrderId;
+          orderImportObjCopy.DOCUMENTNO = 1;
+          orderImportObjCopy.ISAID = 'ZINUS.COM';
+          orderImportObjCopy.SHIPTO = '';
+          orderImportObjCopy.SHPNAME = orderImport.shipping_address_name;
+          orderImportObjCopy.SHPADDR1 = orderImport.shipping_address_1;
+          orderImportObjCopy.SHPADDR2 = orderImport.shipping_address_2;
+          orderImportObjCopy.SHPADDR3 = '';
+          orderImportObjCopy.SHPADDR4 = '';
+          orderImportObjCopy.SHPCITY = orderImport.shipping_address_city;
+          orderImportObjCopy.SHPSTATE = orderImport.shipping_address_state;
+          orderImportObjCopy.SHPZIP = orderImport.shipping_address_zip;
+          orderImportObjCopy.SHPCOUNTRY = orderImport.shipping_address_country;
+          orderImportObjCopy.SHPPHONE = orderImport.shipping_address_phone;
+          orderImportObjCopy.SHPEMAIL = orderImport.contact_email;
+          orderImportObjCopy.PONUMBER = orderImport.zinus_po;
+          orderImportObjCopy.REFERENCE = '';
+          orderImportObjCopy.ORDDATE = moment(orderImport.created_at).format("MM/DD/YYYY");
+          orderImportObjCopy.TD503 = 'FedEx Ground';
+          orderImportObjCopy.TD505 = '';
+          orderImportObjCopy.TD512 = '';
+          orderImportObjCopy.EXPDATE = moment(orderImport.created_at).add(5, 'day').format("MM/DD/YYYY");
+          orderImportObjCopy.DELVBYDATE = moment(orderImport.created_at).add(10, 'day').format("MM/DD/YYYY");
+          orderImportObjCopy.WHCODE = '';
+          orderImportObjCopy.STATUS = 0;
+          orderImportObjCopy.OPTORD01 = orderImport.order_number;
+          orderImportObjCopy.OPTORD02 = orderImport.total_price;
+          orderImportObjCopy.OPTORD03 = orderImport.subtotal_price;
+          orderImportObjCopy.OPTORD04 = orderImport.total_tax;
+          orderImportObjCopy.OPTORD05 = orderImport.discount_codes_type + ": " + orderImport.discount_codes_code;
+          orderImportObjCopy.OPTORD06 = orderImport.discount_codes_amount;
+          orderImportObjCopy.OPTORD07 = orderImport.discount_fixed_amount;
+          orderImportObjCopy.OPTORD08 = orderImport.total_discounts;
+          orderImportObjCopy.OPTORD09 = '';
+          orderImportObjCopy.OPTORD10 = '';
+          orderImportObjCopy.OPTORD11 = '';
+          orderImportObjCopy.OPTORD12 = '';
+          orderImportObjCopy.OPTORD13 = '';
+          orderImportObjCopy.OPTORD14 = '';
+          orderImportObjCopy.OPTORD15 = '';
+          orderImportObjCopy.LINENUM = orderImport.line_items_index;
+          orderImportObjCopy.ITEM = orderImport.line_items.sku;
+          orderImportObjCopy.QTYORDERED = orderImport.line_items.qty;
+          orderImportObjCopy.ORDUNIT = 'ea';
+          orderImportObjCopy.UNITPRICE = orderImport.cart_pricing.unit_price;
+          orderImportObjCopy.OPTITM01 = orderImport.line_items.tax_price;
+          orderImportObjCopy.OPTITM02 = orderImport.line_items.recycling_fee;
+          orderImportObjCopy.OPTITM03 = orderImport.cart_pricing.cart_discount_percent_amount;
+          orderImportObjCopy.OPTITM04 = orderImport.cart_pricing.cart_price;
+          orderImportObjCopy.OPTITM05 = orderImport.cart_pricing.regular_price;
+          orderImportObjCopy.OPTITM06 = '';
+          orderImportObjCopy.OPTITM07 = '';
+          orderImportObjCopy.OPTITM08 = '';
+          orderImportObjCopy.OPTITM09 = '';
+          orderImportObjCopy.OPTITM10 = '';
+          importList.push(orderImportObjCopy);
+        }
+        // Convert json objects to csv and write in Import
+        j2c.json2csv(importList, j2cCallbackImport);
       };
     } // end of request callback
 )}; // end of getOrders funciton
 
-recallOrderNo();
+recallOrderId();
